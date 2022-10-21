e300a5b8d2a7efae05d63d6807d377fb060c125c
ZYX31 <jasiusyl@gmail.com>
2022-10-22 00:16:43 +0200

    Skonczylem na 6 odcinku https://www.youtube.com/watch?v=X3-6hLRI4do

--- app/src/main/java/com/example/pierwszaapka/MainActivity.kt
+++ app/src/main/java/com/example/pierwszaapka/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.pierwszaapka
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.Intent.ACTION_VIEW
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import java.time.Duration
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -15,13 +18,22 @@ class MainActivity : AppCompatActivity() {
         val buttonF = findViewById<Button>(R.id.ButtonF)
         val buttonS = findViewById<Button>(R.id.ButtonS)
         buttonF.setOnClickListener{
+
             buttonF.text = "Działa!"
-            var message = Toast.makeText(applicationContext,"Raz Mi daje raz zabiera!",Toast.LENGTH_LONG)
+            val message = Toast.makeText(applicationContext,"Raz Mi daje raz zabiera!",Toast.LENGTH_LONG)
             message.show()
+
+            //Tworzymy nowa aktywnosc odpalajaca SecondActivity
+            var nowaAktywnosc: Intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(nowaAktywnosc)
         }
         buttonS.setOnClickListener{
-            var message = Toast.makeText(applicationContext,"Los chce ze mna grac w pokera!",Toast.LENGTH_SHORT)
+            val message = Toast.makeText(applicationContext,"Los chce ze mna grac w pokera!",Toast.LENGTH_SHORT)
             message.show()
+
+            val adress = "https://pl.wikipedia.org/wiki/Zenon_Martyniuk"
+            val channelRevolShen = Intent(ACTION_VIEW, Uri.parse(adress))
+            startActivity(channelRevolShen)
         }
 
     }
